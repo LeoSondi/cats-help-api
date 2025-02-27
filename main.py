5333a7ea79eb109d8fc406906d1f923319e653b8
leosondi <vrnvskdnl@gmail.com>
2025-02-27 20:04:48 +0600

    Настройка CORS

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Разрешенные домены
+    allow_origins=["http://localhost:5173"],  # Разрешенные домены
     allow_credentials=True,
     allow_methods=["*"],  # Разрешенные методы
     allow_headers=["*"],  # Разрешенные заголовки
